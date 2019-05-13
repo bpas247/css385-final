fc4ec6d79c10f048dd3a2f5e59f0783c4bd17f64
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-13 15:55:48 -0700

    create the player model and remove model taken from the asset store
    also includes preliminary player controls and updated scene

--- src/Assets/Scripts/PlayerScript.cs
+++ src/Assets/Scripts/PlayerScript.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+	public float movementSpeed, rotateSpeed;
 
-    // Update is called once per frame
-    void Update()
+	private void MoveEffect(KeyCode code, Vector3 movement)
+	{
+		if (Input.GetKey(code))
+		{
+			transform.Find("Pelvis").GetComponent<Rigidbody>().AddForce(movement * movementSpeed, ForceMode.Impulse);
+			transform.Find("Body").GetComponent<Rigidbody>().AddForce(movement * movementSpeed, ForceMode.Impulse);
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-    }
+		MoveEffect(KeyCode.W, Vector3.forward);
+		MoveEffect(KeyCode.S, Vector3.back);
+		MoveEffect(KeyCode.A, Vector3.left);
+		MoveEffect(KeyCode.D, Vector3.right);
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Find("Body").Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Find("Body").Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+		}
+		//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//Plane plane = new Plane(Vector3.up, Vector3.zero);
+		//float distance;
+		//if (plane.Raycast(ray, out distance))
+		//{
+		//	Vector3 target = ray.GetPoint(distance);
+		//	Vector3 direction = target - transform.position;
+		//	float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+		//	//transform.rotation = Quaternion.Euler(0, rotation, 0);
+		//	//transform.Find("Head").rotation = Quaternion.Euler(0, rotation, 0);
+		//	//transform.Find("Pelvis").rotation = Quaternion.Euler(0, rotation, 0);
+		//}
+	}
 }
