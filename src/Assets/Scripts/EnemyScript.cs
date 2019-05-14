ae9a527fe77441a827127bf324df1640fe686efc
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-14 12:48:46 -0700

    Update fight scene to include enemy
    also made a prefab out of the human model, and created a preliminary
    Enemy Script.

---
+++ src/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+	public float movementSpeed;
+	private Transform player, myBody;
+	// Start is called before the first frame update
+	void Start()
+    {
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		myBody = transform.Find("Body");
+		player = GameObject.FindGameObjectWithTag("Player").transform.Find("Body");
+		myBody.transform.position = Vector3.MoveTowards(myBody.transform.position, player.position, movementSpeed * Time.deltaTime);
+	}
+}
