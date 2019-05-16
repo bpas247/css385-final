24a2fbc57d0d329393f54a25d2509f62e427c59c
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-15 23:56:38 -0700

    finishing touches on the fight scene demo and preliminary draft of navigation
    also added a sword asset and smoothed out player controls,
    disabled enemy AI (for now)

--- src/Assets/Scripts/EnemyScript.cs
+++ src/Assets/Scripts/EnemyScript.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-	public float movementSpeed;
+	public float movementSpeed, rotateSpeed;
+	public int health;
+	public bool isDead = false;
 	private Transform player, myBody;
 	// Start is called before the first frame update
 	void Start()
@@ -15,8 +17,22 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		if(health <= 0)
+		{
+			Debug.Log("Death");
+			Destroy(gameObject);
+		}
+
 		myBody = transform.Find("Body");
 		player = GameObject.FindGameObjectWithTag("Player").transform.Find("Body");
-		myBody.transform.position = Vector3.MoveTowards(myBody.transform.position, player.position, movementSpeed * Time.deltaTime);
+
+
+		//Vector3 movement = Vector3.MoveTowards(myBody.transform.position, player.position, movementSpeed * Time.deltaTime);
+		//myBody.transform.position = movement;
+		//transform.Find("Pelvis").position = movement;
+		
+		//Vector3 direction = Random.value > 0.5f ? Vector3.up : -Vector3.up;
+		//transform.Find("Pelvis").Rotate(direction * rotateSpeed * Time.deltaTime);
+		//transform.Find("Body").Rotate(direction * rotateSpeed * Time.deltaTime);
 	}
 }
--- src/Assets/Scripts/PlayerScript.cs
+++ src/Assets/Scripts/PlayerScript.cs
@@ -16,6 +16,15 @@ public class PlayerScript : MonoBehaviour
 		}
 	}
 
+	private void RotateEffect(KeyCode code, Vector3 movement)
+	{
+		if (Input.GetKey(code))
+		{
+			transform.Find("Pelvis").Rotate(movement * rotateSpeed * Time.deltaTime);
+			transform.Find("Body").Rotate(movement * rotateSpeed * Time.deltaTime);
+		}
+	}
+
 	// Update is called once per frame
 	void Update()
     {
@@ -24,25 +33,7 @@ public class PlayerScript : MonoBehaviour
 		MoveEffect(KeyCode.A, Vector3.left);
 		MoveEffect(KeyCode.D, Vector3.right);
 
-		if(Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.Find("Body").Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
-		}
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.Find("Body").Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
-		}
-		//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		//Plane plane = new Plane(Vector3.up, Vector3.zero);
-		//float distance;
-		//if (plane.Raycast(ray, out distance))
-		//{
-		//	Vector3 target = ray.GetPoint(distance);
-		//	Vector3 direction = target - transform.position;
-		//	float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-		//	//transform.rotation = Quaternion.Euler(0, rotation, 0);
-		//	//transform.Find("Head").rotation = Quaternion.Euler(0, rotation, 0);
-		//	//transform.Find("Pelvis").rotation = Quaternion.Euler(0, rotation, 0);
-		//}
+		RotateEffect(KeyCode.LeftArrow, -Vector3.up);
+		RotateEffect(KeyCode.RightArrow, Vector3.up);
 	}
 }
