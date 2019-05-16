66725ddc4de16a92c5ddf449aa69fa6a284f0bc9
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-16 10:20:10 -0700

    potential fix for camera not following player

--- src/Assets/Scripts/CameraScript.cs
+++ src/Assets/Scripts/CameraScript.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-	Transform player;
-    // Start is called before the first frame update
-    void Start()
-    {
-		player = GameObject.FindGameObjectWithTag("Player").transform.Find("Head");
-	}
+	public Transform player;
 
     // Update is called once per frame
     void Update()
