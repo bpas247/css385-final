11dbcc480bea66c68a76f130341ec63dedaf71bc
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-15 23:59:23 -0700

    Add fight scene prototype (#12)
    * fix player model's body parts so the ragdoll stays up
    
    also add init commit for fight scene prototype
    
    * create the player model and remove model taken from the asset store
    
    also includes preliminary player controls and updated scene
    
    * Update fight scene to include enemy
    
    also made a prefab out of the human model, and created a preliminary
    Enemy Script.
    
    * finishing touches on the fight scene demo and preliminary draft
    of navigation
    
    also added a sword asset and smoothed out player controls,
    disabled enemy AI (for now)

---
+++ src/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+	Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player").transform.Find("Head");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+}
