79761ea77057b3572ccf094bac7cafcd0490c49e
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-19 02:43:54 -0700

    fix: runtime exceptions being thrown and update production files

--- src/Assets/Scripts/EnemyScript.cs
+++ src/Assets/Scripts/EnemyScript.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
 	public float movementSpeed, rotateSpeed;
-	private Transform player, myMovingPart;
+    public Transform player;
+    private Transform[] myMovingParts;
 	// Start is called before the first frame update
 	void Start()
     {
+        myMovingParts = new[]{ transform.Find("Body"), transform.Find("Body").Find("Right Arm")};
 
-	}
+    }
 
     // Update is called once per frame
     void Update()
     {
-		myMovingPart = transform.Find("Body").Find("Right Arm");
-		player = GameObject.FindGameObjectWithTag("Player").transform.Find("Body");
 
-        Vector3 movement = player.position - myMovingPart.position;
 
-        movement = new Vector3(movement.x < 0 ? -1 : 1, movement.y, movement.z < 0 ? -1 : 1);
 
-        if(GetComponent<AttributesScript>().GetValue(AttributesScript.ATTRIBUTES.HEALTH) > 0)
-            myMovingPart.GetComponent<Rigidbody>().AddForce(movement * movementSpeed, ForceMode.Impulse);
+        if (GetComponent<AttributesScript>().GetValue(AttributesScript.ATTRIBUTES.HEALTH) > 0)
+        {
+            foreach(Transform myMovingPart in myMovingParts)
+            {
+                Vector3 movement = player.position - myMovingPart.position;
+
+                movement = new Vector3(movement.x < 0 ? -1 : 1, movement.y, movement.z < 0 ? -1 : 1);
+
+                myMovingPart.GetComponent<Rigidbody>().AddForce(movement * movementSpeed, ForceMode.Impulse);
+            }
+        }
     }
 }
