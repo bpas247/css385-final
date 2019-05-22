39cabc03411ea1b077efaaff228a1349ff4e9a1d
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-22 11:42:59 -0700

    Alpha inventory (#21)
    * Initial working inventory and weapon pickup
    
    * Edit scene
    
    * Fixed inventory bug

---
+++ src/Assets/Scripts/InventroyScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventroyScript : MonoBehaviour
+{
+    public GameObject inventory;
+
+    private int allSlots;
+    private GameObject[] slots;
+
+    public GameObject slotHolder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allSlots = 8;
+        slots = new GameObject[allSlots];
+
+        for(int i = 0; i < allSlots; i++)
+        {
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+
+            if(slots[i].GetComponent<Slot>().item == null)
+            {
+                slots[i].GetComponent<Slot>().empty = true;
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventory.SetActive(!inventory.activeSelf);
+        }
+    }
+
+    public void itemClicked(GameObject other)
+    {
+        if(other.tag == "Weapon")
+        {
+            GameObject itemPickedUp = other;
+            ItemDropScript item = itemPickedUp.GetComponent<ItemDropScript>();
+
+            AddItem(itemPickedUp, item.weaponDamage, item.weaponSpeed, item.weaponRange, item.icon);
+        }
+    }
+
+    void AddItem(GameObject itemPickedUp, int weaponDamage, double weaponSpeed, double weaponRange, Texture2D icon)
+    {
+        for (int i = 0; i < allSlots; i++)
+        {
+            if(slots[i].GetComponent<Slot>().empty)
+            {
+                itemPickedUp.GetComponent<ItemDropScript>().pickedUp = true;
+
+                slots[i].GetComponent<Slot>().item = itemPickedUp;
+                slots[i].GetComponent<Slot>().icon = icon;
+                slots[i].GetComponent<Slot>().weaponDamage = weaponDamage;
+                slots[i].GetComponent<Slot>().weaponSpeed = weaponSpeed;
+                slots[i].GetComponent<Slot>().weaponRange = weaponRange;
+
+                itemPickedUp.transform.parent = slots[i].transform;
+                itemPickedUp.SetActive(false);
+
+                slots[i].GetComponent<Slot>().UpdateSlot();
+                slots[i].GetComponent<Slot>().empty = false;
+                break;
+            }
+        }
+    }
+}
---
+++ src/Assets/Scripts/Slot.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject item;
+    public bool empty;
+    public Texture2D icon;
+
+    public int weaponDamage = 0;
+    public double weaponSpeed = 0;
+    public double weaponRange = 0;
+
+    string tooltipText = "Attack: 1\nSpeed: 1\nRange: 1";
+    private string currentTooltipText = "";
+    GUIStyle guiStyleFore;
+    GUIStyle guiStyleBack;
+
+    void Start()
+    {
+        guiStyleFore = new GUIStyle();
+        guiStyleFore.normal.textColor = Color.white;
+        guiStyleFore.alignment = TextAnchor.UpperCenter;
+        guiStyleFore.wordWrap = true;
+        guiStyleFore.fontSize = 24;
+        guiStyleBack = new GUIStyle();
+        guiStyleBack.normal.textColor = Color.black;
+        guiStyleBack.alignment = TextAnchor.UpperCenter;
+        guiStyleBack.wordWrap = true;
+        guiStyleBack.fontSize = 24;
+
+        tooltipText = "Attack: " + weaponDamage + "\nSpeed: " + weaponSpeed + "\nRange: " + weaponRange;
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    public void UpdateSlot()
+    {
+        this.GetComponent<Image>().sprite = Sprite.Create(icon, new Rect(0, 0, icon.width, icon.height) , new Vector2(0.5f, 0.5f));
+    }
+
+    // Update is called once per frame
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(empty == false)
+        {
+            tooltipText = "Attack: " + weaponDamage + "\nSpeed: " + weaponSpeed + "\nRange: " + weaponRange;
+            currentTooltipText = tooltipText;
+        }
+    }
+    
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        currentTooltipText = "";
+    }
+
+    private void OnGUI()
+    {
+        if (currentTooltipText != "")
+        {
+            float x = Event.current.mousePosition.x;
+            float y = Event.current.mousePosition.y;
+            GUI.Label(new Rect(x - 149, y + 21, 300, 60), currentTooltipText, guiStyleBack);
+            GUI.Label(new Rect(x - 150, y + 20, 300, 60), currentTooltipText, guiStyleFore);
+        }
+    }
+}
