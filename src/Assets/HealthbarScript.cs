0154bdbdef862567666fb808b668da1f5a70f4d3
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-23 12:15:19 -0700

    Healthbar ui (#23)
    * Healthbar updated
    
    * Fixed healthbar

---
+++ src/Assets/HealthbarScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthbarScript : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("MainPlayer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(player.GetComponent<AttributesScript>().GetValue(AttributesScript.ATTRIBUTES.HEALTH));
+        GetComponent<Image>().fillAmount = ( player.GetComponent<AttributesScript>().GetValue(AttributesScript.ATTRIBUTES.HEALTH) / 3.0f);
+    }
+}
