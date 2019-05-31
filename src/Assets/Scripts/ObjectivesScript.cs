ebe438fabd3ced330d9b78e5821de6a587c924a9
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-05-30 20:13:34 -0700

    feat: add objective indicator, rename objective to be generic
    Also, update scenes to reflect new behavior

---
+++ src/Assets/Scripts/ObjectivesScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectivesScript : MonoBehaviour
+{
+	public int enemiesRemaining;
+	public GameObject currentObjective;
+	public GameObject nextRoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+	GameObject findClosestEnemy()
+	{
+		GameObject outVal = null;
+		float outValDist = float.MaxValue;
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+		foreach (GameObject enemy in enemies)
+		{
+			float delta = Vector3.Distance(Camera.main.transform.position, enemy.transform.position);
+			bool isAlive = enemy.GetComponent<AttributesScript>().GetValue(AttributesScript.ATTRIBUTES.HEALTH) > 0;
+			if (delta < outValDist && isAlive)
+			{
+				outVal = enemy;
+				outValDist = delta;
+			}
+		}
+
+		return outVal;
+	}
+
+	int CalculateEnemiesRemaining()
+	{
+		int count = 0;
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		foreach(GameObject enemy in enemies)
+		{
+			if(enemy.GetComponent<AttributesScript>().GetValue(AttributesScript.ATTRIBUTES.HEALTH) > 0)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		enemiesRemaining = CalculateEnemiesRemaining();
+		if(enemiesRemaining > 0)
+		{
+			transform.Find("Text").gameObject.GetComponent<Text>().text = "Enemies Remaining: " + enemiesRemaining;
+			currentObjective = findClosestEnemy();
+		}
+		else
+		{
+			transform.Find("Text").gameObject.GetComponent<Text>().text = "Objectives Complete";
+			currentObjective = nextRoom;
+		}
+	}
+}
